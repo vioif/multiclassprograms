2e5402ab0bd2d8a2caf529671def49ab8ff8b4b1
vioif <ireneaflores3@gmail.com>
2018-01-02 15:42:04 -0500

    items in cart

--- cart.rb
+++ cart.rb
@@ -1,1 +1,43 @@
-require "./products.rb"
+require "./product.rb"
+
+
+class Cart
+  @items = []
+
+  def initialize
+  end
+
+  def add_product(product)
+    @@items << product
+  end
+
+  def remove_product
+    @items.each { |item|
+      if (item == product)
+        @@items.delete(product)
+        return true
+      end
+    }
+        return false
+      end
+
+  def before_total
+    total = 0
+    @@items.each { |item|
+      total += item.price
+    }
+    return total
+  end
+
+  def total_after_item_added
+    total = 0
+    @items.each { |item|
+      total += item.total
+    }
+    return total
+  end
+
+  def cart_items
+    return @@items
+  end
+end
--- main.rb
+++ main.rb
@@ -1,2 +1,22 @@
-require "./products"
+require "./product"
 require "./cart.rb"
+
+
+orange = Product.new("orange",5.00,0.13)
+cheese = Product.new("cheeser",6.75,0.13)
+bread = Product.new("bread",4.50,0.13)
+
+cart = Cart.new
+
+cart.add_product(orange)
+cart.add_product(bread)
+cart.add_product(chedder)
+cart.add_product(chedder)
+
+p cart.cart_items
+
+puts cart.before_total
+puts total_after_item_added
+
+cart.remove_product(chedder)
+p cart.cart_items
